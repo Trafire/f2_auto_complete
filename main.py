444c2b25659934f583837bbcbdb2edb82c2c0b39
Antoine Wood <antoinewood@gmail.com>
2019-12-13 16:21:49 -0500

    Auto start on computer login

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import getpass, os, sys
 
 
-if __name__=='__main__':
-    active_window_handle = None
+def add_to_startup(executable, file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' %  getpass.getuser()
+    print(bat_path)
+    with open(bat_path + '\\' + "f2_connection1.bat", "w+") as bat_file:
+        bat_file.write(rf'"{executable}" "{file_path}.main.py"')
+        bat_file.write("\n")
+        bat_file.write(rf'"{file_path}/database/cloud_sql_proxy.exe" -instances="fmc-crm-252016:northamerica-northeast1:fmc-crm-db"=tcp:3306')
+
+    with open(bat_path + '\\' + "f2_connection2.bat", "w+") as bat_file:
+        bat_file.write(rf'"{executable}" "{file_path}/main.py"')
+        bat_file.write("\npause")
+
+
+
+#database_cmd ='"database/cloud_sql_proxy.exe" -instances="fmc-crm-252016:northamerica-northeast1:fmc-crm-db"=tcp:3306'
+add_to_startup(sys.executable)
+print('test')
+
+
