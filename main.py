460df118aa55639ab3870baafc90aa7cc9b8c432
Antoine Wood <antoinewood@gmail.com>
2019-12-19 13:38:09 -0500

    Update main.py
    testing 3

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def add_to_startup(executable, file_path=""):
 #add_to_startup(sys.executable)
 
 # log into system
-print("TESTING 2222")
+print("TESTING 3333")
 if __name__ == '__main__' and False:
     username = f2_password['username']
     password = f2_password['password']
