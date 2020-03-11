ce09467c4482afcb9e82f6d9f8e5c514e7ffdf0f
Antoine Wood <antoinewood@gmail.com>
2020-03-11 17:29:48 -0400

    Fix bug if ksweker not METZCA

--- verification/f2_page.py
+++ verification/f2_page.py
@@ -1,8 +1,10 @@
-from verification.reference import VERIFICATION
-from verification import f2
+import copy
+
 from interface import window
 from parse import parse
-import copy
+from verification import f2
+from verification.reference import VERIFICATION
+
 
 def verify_per_location(location):
     keyword = 'stock_per_location-flowers-location'
@@ -14,6 +16,7 @@ def verify_per_location(location):
     for w in window_data:
         w['target'] = swap_values(swaps, w['target'])
     return f2.verify(window_data, 50)
+
 
 def verify_per_location_virtual(location):
     keyword = 'virtual_stock_per_location-flowers-location'
@@ -28,15 +31,15 @@ def verify_per_location_virtual(location):
 
 
 def verify_per_location_price_level(location, level, attempt=0):
+
     if attempt > 50:
         return False
-
     level = str(level)
-    print(level)
     if level == '0':
-        row = parse.process_scene(window.get_window())[4][50:-5].strip()
-        print(row)
-        if row == '':
+        row = parse.process_scene(window.get_window())[4][55:-5].strip()
+        print('row', row)
+        if row == '' or row == 'Pricegroup :    0':
+            print("TRUE")
             return True
         else:
             return verify_per_location_price_level(location, level, attempt + 1)
@@ -57,7 +60,7 @@ def verify_lot_info(lot_number, virtual=False):
     else:
         screen = 'lot_info'
 
-    window_data = copy.deepcopy(VERIFICATION['screens'] [screen])
+    window_data = copy.deepcopy(VERIFICATION['screens'][screen])
     swaps = {
         '{lot_number}': lot_number,
     }
@@ -74,18 +77,16 @@ def swap_values(swaps, text):
     return text
 
 
-
-
 if __name__ == '__main__':
     location = 'ec'
     level = '2'
-    #cmd = verify_per_location_price_level(location, level)
-    #print(cmd)
-    #print(verify_lot_info('641921'))
-    #print(verify_lot_info('641913'))
+    # cmd = verify_per_location_price_level(location, level)
+    # print(cmd)
+    # print(verify_lot_info('641921'))
+    # print(verify_lot_info('641913'))
 
     a = f2.verify([{'target': 'Intern partijnummer  : 647801', 'location': (17, 97)}], 10)
-    #b =  f2.verify([{'target': 'Intern partijnummer  : 647801', 'location': (18, 97)}], 10)
+    # b =  f2.verify([{'target': 'Intern partijnummer  : 647801', 'location': (18, 97)}], 10)
 
     print(a)
 
